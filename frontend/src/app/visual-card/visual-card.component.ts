4f19db6d05fbd5248ff02de03d668257c84d9b23
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2023-06-01 18:56:21 +0200

    sreach of header

---
+++ frontend/src/app/visual-card/visual-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visual-card',
+  templateUrl: './visual-card.component.html',
+  styleUrls: ['./visual-card.component.css']
+})
+export class VisualCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
