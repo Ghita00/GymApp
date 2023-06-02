6ae94209a41a33433e6cc4ada7576df05f3e6856
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2023-06-02 09:10:45 +0200

    added routing, 404 page and created profile component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { Page404Component } from './page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'userManagment', component: ProfileComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
+  {path: '**', component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { VisualCardComponent } from './visual-card/visual-card.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { Page404Component } from './page404/page404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    VisualCardComponent
+    VisualCardComponent,
+    ProfileComponent,
+    HomeComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
