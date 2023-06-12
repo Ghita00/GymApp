dd5dae16472d6de17e91f20233d47f5d4c4ee5e1
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2023-06-12 22:58:12 +0200

    fixed resposives, added new scroll bar style and add logic to button of forms

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  seeMenu : boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  openMenu() {
+    this.seeMenu = this.seeMenu ? false : true; 
+  }
 }
