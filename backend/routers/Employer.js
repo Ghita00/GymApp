4c478ccfbb8667d2b70a806c304dfe4b995ef3b8
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2023-07-04 21:31:11 +0200

    add login system

--- backend/routers/Employer.js
+++ backend/routers/Employer.js
@@ -39,36 +39,21 @@ const schema = new mongoose.Schema({
 //employ model
 const model = mongoose.model("employ", schema);
 
-async function insertUser(){
-  console.log("Test insert");
-  await new model({
-    name: "test",
-    user: "spero funzioni",
-    password:"aaa",
-    userManagment: []
-  }).save();
-}
-
 router.get("/allEmploys", async (req, res) => {
-    //insertUser();
-    //res.send(employs);
     res.send(await model.find());
 })
 
-router.post("/login",jsonParser, (req, res) => {
-  if(req.body.user && req.body.password){
-    for(let employ of employs){
-      if(employ.user == req.body.user){
-        if(employ.password == req.body.password){
-          res.send({connect: true, message: "user found"});
-        }else{
-          res.send({connect: false,message: "user not found"});
-        }
-      }
-    }
+router.post("/login",jsonParser, async (req, res) => {
+  let userFound = await model.find({
+    user: req.body.user,
+    password: req.body.password
+  })
+  console.log(userFound)
+  if(userFound.length != 0){
+    res.send({connect: true, message: "user found"});
+  }else{
     res.send({connect: false,message: "user not found"});
   }
-  res.send({connect: false,message: "user not found"});
 })
 
 module.exports = router;
