de0e5cab364791882a9e49fdf19fe7d5c7827298
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2023-09-13 23:20:34 +0200

    completed curd for card and employer. Todo will write a document with all API + descriptions

--- backend/routers/Card.js
+++ backend/routers/Card.js
@@ -26,8 +26,7 @@ router.get("/allCards", async (req, res) => {
 //GET: RETURN ALL CARD INFORMATION
 router.get("/detailCard", jsonParser, async (req, res) => {
   let idCard = req.body.idCard;
-  console.log(idCard)
-  let cardDetail = await model.findById(idCard);
+  let cardDetail = await model.find({_id:new ObjectId(idCard)});
 
   if(!cardDetail || cardDetail.length == 0){
       res.status(500).send({message: "an error occurred"});
@@ -45,5 +44,38 @@ router.post("/createCard", jsonParser, async (req, res) => {
     res.status(500).send({message: "an error occurred"});
   }
 });
+
+//PUT: UPDATE INFORMATION OF CARD   
+router.put("/updateCard", jsonParser, async (req, res) => {
+  let idCard = req.body.newCard._id;
+  console.log(idCard)
+  let oldCard = await model.find({_id:new ObjectId(idCard)});
+  await model.deleteOne({_id:new ObjectId(idCard)});
+
+  if(!oldCard){
+    res.status(500).send({message: "an error occurred"});
+  }
+
+  let newCard = await model.insertMany([req.body.newCard]);
+  if(newCard){
+    res.status(200).send({message: newCard });
+  }else{
+    res.status(500).send({message: "an error occurred"});
+  }
+})
+
+//REMOVE: REMOVE CARD FROM COLLECTION   
+router.delete("/deleteCard", jsonParser, async (req, res) => {
+  let idCard = req.body.idCard;
+  console.log(idCard)
+  let oldCard = await model.find({_id:new ObjectId(idCard)});
+  await model.deleteOne({_id:new ObjectId(idCard)});
+
+  if(!oldCard || oldCard.length == 0){
+    res.status(500).send({message: "an error occurred"});
+  }else{
+    res.status(200).send(oldCard);
+  } 
+})
 
 module.exports = router;
--- backend/routers/Employer.js
+++ backend/routers/Employer.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const jsonParser = bodyParser.json();
+const ObjectId = require('mongodb').ObjectId;
 
 //EMPLOY SCHEMA
 const schema = new mongoose.Schema({
@@ -13,19 +14,103 @@ const schema = new mongoose.Schema({
 });
 
 //EMPLOY MODEL
-const model = mongoose.model("employ", schema);
+const model = mongoose.model("employer", schema);
 
 //POST: TRIED LOGIN AND RETURN IF CREDENTIALS ARE VALID
-router.post("/login",jsonParser, async (req, res) => {
+router.post("/login", jsonParser, async (req, res) => {
+  console.log({
+    user: req.body.user,
+    password: req.body.password
+  })
   let userFound = await model.find({
     user: req.body.user,
     password: req.body.password
   })
   console.log(userFound)
-  if(userFound.length != 0){
-    res.status(200).send({message: "user found"});
+  if(userFound && userFound.length > 0){
+    res.status(200).send({message: userFound});
   }else{
     res.status(500).send({message: "user not found"});
+  }
+})
+
+//POST: CREATE EMPLOYED
+router.post("/createEmployer", jsonParser, async(req, res) => {
+  let newEmployer = await model.insertMany(req.body.newUser)
+  if(newEmployer && newEmployer.length > 0){
+    res.status(200).send({message: newEmployer});
+  }else{
+    res.status(500).send({message: "an error occurred"});
+  }
+})
+
+//PUT: ADD USER MANAGMENT
+router.put("/addUserManagment", jsonParser, async(req, res) => {
+  let idEmployer = req.body.idEmployer;
+  let idUser = req.body.idUser;
+
+  let employer = await model.findById({_id:new ObjectId(idEmployer)})
+  if(!employer || employer.length == 0){
+    res.status(500).send({message: "an error occurred"});
+  }else{
+    employer.userManagment.push(idUser)
+    await model.deleteOne({_id:new ObjectId(idEmployer)}); 
+    let newUserManagment = await model.insertMany(employer); 
+
+    if(newUserManagment && newUserManagment.length > 0){
+      res.status(200).send({message: employer});
+    }else{
+      res.status(500).send({message: "an error occurred"});
+    }
+  }
+  
+})
+
+//PUT: CHANGE EMPLOYER INFORMATION
+router.put("/changeEmployerInformation", jsonParser, async(req, res) => {
+  let newEmployer = req.body.newEmployer;
+  let oldEmployer = await model.findById({_id:new ObjectId(newEmployer._id)})
+  await model.deleteOne(oldEmployer); 
+  let newEmployerInsert = await model.insertMany(newEmployer);
+
+  if(newEmployerInsert && newEmployerInsert.length > 0){
+    res.status(200).send({message: newEmployerInsert});
+  }else{
+    res.status(500).send({message: "an error occurred"});
+  }
+})
+
+//DELETE: REMOVE USER MANAGMENT
+router.delete("/removeUserManagment", jsonParser, async(req, res) => {
+  let idEmployer = req.body.idEmployer;
+  let idUser = req.body.idUser;
+  let employer = await model.findById({_id:new ObjectId(idEmployer)})
+  
+  if(!employer || employer.length == 0){
+    res.status(500).send({message: "an error occurred"});
+  }else{
+    employer.userManagment = employer.userManagment.filter(id => id != idUser)
+    await model.deleteOne({_id:new ObjectId(idEmployer)});
+    let newUserManagment = await model.insertMany(employer); 
+
+    if(newUserManagment && newUserManagment.length > 0){
+      res.status(200).send({message: employer});
+    }else{
+      res.status(500).send({message: "an error occurred"});
+    }
+  }
+})
+
+//DELETE: REMOVE EMPLOYER
+router.delete("/removeEmployer", jsonParser, async(req, res) => {
+  let idEmployer = req.body.idEmployer;
+  let oldEmployer = await model.find({_id:new ObjectId(idEmployer)})
+  await model.deleteOne({_id:new ObjectId(idEmployer)}); 
+
+  if(oldEmployer && oldEmployer.length > 0){
+    res.status(200).send({message: oldEmployer});
+  }else{
+    res.status(500).send({message: "an error occurred"});
   }
 })
 
